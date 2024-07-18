0c90b934562b29c968f2c32345d73fe691f60ad6
lmoheyma <louismoheymani@gmail.com>
2024-07-19 00:23:10 +0200

    ⏳ : bug vector type

--- ex00/utils.rs
+++ ex00/utils.rs
@@ -1,36 +1,47 @@
-struct Vector::<K> {
-	//var
+use std::ops::{AddAssign, SubAssign, MulAssign};
+
+pub struct Vector<K> {
+	x: K,
+	y: K,
 }
 
-struct Matrix::<K> {
-	//var
+// struct Matrix::<K> {
+// 	//var
+// }
+
+
+impl<K> Vector<K>
+where
+	K: AddAssign + SubAssign + MulAssign + Copy,	
+{
+	pub fn from(arr: [K; 2]) -> Self {
+        Vector { x: arr[0], y: arr[1] }
+    }
+
+	pub fn add(&mut self, v: &Vector<K>) {
+		self.x += v.x;
+		self.y += v.y;
+	}
+
+	// fn sub(&mut self, v: &Vector<K>) {
+
+	// }
+
+	// fn scl(&mut self, a: K) {
+
+	// }
 }
 
-
-impl<K> Vector<K> {
-	fn add(&mut self, v: &Vector<K>) {
+// impl<K> Matrix<K> {
+// 	fn add(&mut self, v: &Matrix<K>) {
 		
-	}
+// 	}
 
-	fn sub(&mut self, v: &Vector<K>) {
-
-	}
-
-	fn scl(&mut self, a: K) {
-
-	}
-}
-
-impl<K> Matrix<K> {
-	fn add(&mut self, v: &Matrix<K>) {
+// 	fn sub(&mut self, v: &Matrix<K>) {
 		
-	}
+// 	}
 
-	fn sub(&mut self, v: &Matrix<K>) {
-		
-	}
+// 	fn scl(&mut self, a: K) {
 
-	fn scl(&mut self, a: K) {
-
-	}
-}
+// 	}
+// }
