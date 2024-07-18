ee5f4512d5d9c9f96f85b2ddd946e10356dc3c6e
lmoheyma <louismoheymani@gmail.com>
2024-07-18 23:45:05 +0200

    ⏳ : ex00 + README.md

---
+++ ex00/utils.rs
@@ -0,0 +1,36 @@
+struct Vector::<K> {
+	//var
+}
+
+struct Matrix::<K> {
+	//var
+}
+
+
+impl<K> Vector<K> {
+	fn add(&mut self, v: &Vector<K>) {
+		
+	}
+
+	fn sub(&mut self, v: &Vector<K>) {
+
+	}
+
+	fn scl(&mut self, a: K) {
+
+	}
+}
+
+impl<K> Matrix<K> {
+	fn add(&mut self, v: &Matrix<K>) {
+		
+	}
+
+	fn sub(&mut self, v: &Matrix<K>) {
+		
+	}
+
+	fn scl(&mut self, a: K) {
+
+	}
+}
