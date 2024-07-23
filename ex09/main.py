efa25a143dce433f174f3ff0a98ad6bf4b39d310
lmoheyma <louismoheymani@gmail.com>
2024-07-24 00:40:51 +0200

    ✅ ex09

---
+++ ex09/main.py
@@ -0,0 +1,20 @@
+import sys
+
+sys.path.append("../")
+from cls.Matrix import Matrix
+
+def main():
+	u = Matrix([[1.0, 2.0], [3.0, 4.0]])
+	print(u.transpose())
+
+	u = Matrix([[2.0, -5.0, 0.0],
+				[4.0, 3.0, 7.0],
+				[-2.0, 3.0, 4.0]])
+	print(u.transpose())
+
+	u = Matrix([[-2.0, -8.0, 4.0],
+				[1.0, -23.0, 4.0]])
+	print(u.transpose())
+
+if __name__ == "__main__":
+	main() 
